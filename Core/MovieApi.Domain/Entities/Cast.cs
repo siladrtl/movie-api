de4630a4fa36f0b64594064f014513b4a6fd69db
Sıla Dertli <siladertli39@gmail.com>
2026-07-04 17:31:29 +0300

    Entityler tamamlandı ve Veritabanı bağlantısı yazıldı.

--- Core/MovieApi.Domain/Entities/Cast.cs
+++ Core/MovieApi.Domain/Entities/Cast.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace MovieApi.Domain.Entities
 {
-    internal class Cast
+    public class Cast
     {
+        public int CastId { get; set; }
+        public string Title { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string ImageUrl { get; set; }
+        public string? Overview { get; set; }
+        public string? Biography { get; set; }
+
     }
 }
+
---
+++ Infrastructure/MovieApi.Persistence/Context/MovieApiContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MovieApi.Domain.Entities;
+
+namespace MovieApi.Persistence.Context
+{
+    public class MovieApiContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) //sql bağlantı adresini yazan metot
+        {
+            optionsBuilder.UseSqlServer("Server=siladertli\\SQLEXPRESS; inital Catalog= ApiMovieDb; integrated Security=true;");
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Cast> Casts { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
+    }
+}
